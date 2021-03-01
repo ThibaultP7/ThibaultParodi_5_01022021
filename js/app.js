0221d99f0a5ace03272bd416a7d4305bd9a1195d
ThibaultP7 <thibault.parodi@gmail.com>
2021-03-01 10:43:11 +0100

    Début de la page produit
    Début de la page produit

--- js/app.js
+++ js/app.js
@@ -1,54 +1,124 @@
 var apiErrors = document.getElementById("api-errors");
 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-	if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-		var response = JSON.parse(this.responseText);
+/*Génération de l'URL de l'API selon le choix de produit à vendre
+**********************************************/
 
-		var productsContainer = document.getElementById("products-container");
-		response.forEach((item) => {
-			// Product container
-			var prodContainer = document.createElement("div");
-			prodContainer.setAttribute("id",item._id);
-			prodContainer.setAttribute("class","product-container p-2");
+// const PRODUCTTYPE = "cameras"  //Au choix entre : "cameras" - "furniture" - "teddies"
+// const APIURL = "http://localhost:3000/api/" + PRODUCTTYPE + "/";
 
-			// Product image
-			var prodImgContainer = document.createElement("div");
-			prodImgContainer.setAttribute("id",item._id+"-img");
-			prodImgContainer.setAttribute("class","img-wrapper");
+//id du produit pour permettre un tri dans l'API
 
-			var prodImg = document.createElement("img");
-			prodImg.setAttribute("src",item.imageUrl);
+// let productId = "";
 
-			// Product name
-			var prodName = document.createElement("a");
-			prodName.setAttribute("class","product-link");
-			prodName.setAttribute("href","product.html?id="+item._id);
-			prodName.innerHTML = item.name;
+/*L'utilisateur à besoin d'un panier dans le localStorage de son navigateur
+Vérifier si le panier existe dans le localStorage, sinon le créer et l'envoyer dans le localStorage au premier chargement du site quelque soit la page*/
 
-			// Product price
-			var prodPrice = document.createElement("h3");
-			var price = (item.price/100).toFixed(2);
-			prodPrice.innerHTML = price+"€";
-
-			// Append every element to the div
-			productsContainer.append(prodContainer);
-			var prodContainerId = document.getElementById(item._id);
-			prodContainerId.append(prodImgContainer);
-
-			var prodImgContainerId = document.getElementById(item._id+"-img");
-			prodImgContainerId.append(prodImg);
-
-			prodContainerId.append(prodName);
-			prodContainerId.append(prodPrice);
-		});
-		// console.error(response);
-		// console.log("Connection à l'API réussie");
-		apiErrors.innerHTML = "";
-	} else {
-		// console.log("Erreur de connexion à l'API");
-		apiErrors.innerHTML = '<div class="error">Nous sommes désolé, il y a une erreur lors de la connexion à l\'API.<br>Veuillez vérifier son état.</div>';
-	}
+if (localStorage.getItem("userCart")) {
+	console.log("Panier existant dans le localStorage");
+} else {
+	console.log("Création du panier dans le localStorage");
+	//Le panier est un tableau de produits
+	let cart = [];
+	localStorage.setItem("userCart", JSON.stringify(cart));
 };
-request.open("GET", "http://localhost:3000/api/cameras");
-request.send();
+
+//Tableau et objet demandé par l'API pour la commande
+let contact;
+let products = [];
+
+//L'user a maintenant un panier
+let userCart = JSON.parse(localStorage.getItem("userCart"));
+
+// ---------------------------------
+
+function getProducts() {
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			var response = JSON.parse(this.responseText);
+
+			var productsContainer = document.getElementById("products-container");
+			response.forEach((item) => {
+				// Product container
+				var prodContainer = document.createElement("div");
+				prodContainer.setAttribute("id",item._id);
+				prodContainer.setAttribute("class","product-container p-2");
+
+				// Product image
+				var prodImgContainer = document.createElement("div");
+				prodImgContainer.setAttribute("id",item._id+"-img");
+				prodImgContainer.setAttribute("class","img-wrapper");
+
+				var prodImg = document.createElement("img");
+				prodImg.setAttribute("src",item.imageUrl);
+
+				// Product name
+				var prodName = document.createElement("a");
+				prodName.setAttribute("class","product-link");
+				prodName.setAttribute("href","product.html?id="+item._id);
+				prodName.innerHTML = item.name;
+
+				// Product price
+				var prodPrice = document.createElement("h3");
+				var price = (item.price/100).toFixed(2);
+				prodPrice.innerHTML = price+"€";
+
+				// Append every element to the div
+				productsContainer.append(prodContainer);
+				var prodContainerId = document.getElementById(item._id);
+				prodContainerId.append(prodImgContainer);
+
+				var prodImgContainerId = document.getElementById(item._id+"-img");
+				prodImgContainerId.append(prodImg);
+
+				prodContainerId.append(prodName);
+				prodContainerId.append(prodPrice);
+			});
+			// console.error(response);
+			// console.log("Connection à l'API réussie");
+			apiErrors.innerHTML = "";
+		} else {
+			// console.log("Erreur de connexion à l'API");
+			apiErrors.innerHTML = '<div class="error">Nous sommes désolé, il y a une erreur lors de la connexion à l\'API.<br>Veuillez vérifier son état.</div>';
+		}
+	};
+	request.open("GET", "http://localhost:3000/api/cameras");
+	request.send();
+}
+
+function getProductInfos() {
+	//Collecter l'URL après le ?id= pour le récupérer uniquement sur l'API
+	idProduit = location.search.substring(4);
+	console.log(idProduit);
+
+	var request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+			var response = JSON.parse(this.responseText);
+
+			var productContainer = document.getElementById("product-informations");
+			response.forEach((item) => {
+				// Product container
+				var prodContainer = document.createElement("div");
+				prodContainer.setAttribute("id",item._id);
+				prodContainer.setAttribute("class","product-container p-2");
+
+				// Append every element to the div
+				productsContainer.append(prodContainer);
+				var prodContainerId = document.getElementById(item._id);
+				prodContainerId.append(prodImgContainer);
+
+				var prodImgContainerId = document.getElementById(item._id+"-img");
+				prodImgContainerId.append(prodImg);
+
+				prodContainerId.append(prodName);
+				prodContainerId.append(prodPrice);
+			});
+		} else {
+			// console.log("Erreur de connexion à l'API");
+			apiErrors.innerHTML = '<div class="error">Nous sommes désolé, il y a une erreur lors de la connexion à l\'API.<br>Veuillez vérifier son état.</div>';
+		}
+	};
+	request.open("GET", "http://localhost:3000/api/cameras/"+ idProduit);
+	request.send();
+}
