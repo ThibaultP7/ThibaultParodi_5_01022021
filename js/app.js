9c435fcb406af21d684b8622b80ebafa596444a1
ThibaultP7 <thibault.parodi@gmail.com>
2021-05-03 14:57:02 +0200

    Amélioration avec début de Promise
    Amélioration avec début de Promise

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 var apiErrors = document.getElementById("api-errors");
 
+const productSold = "cameras" // Pour adapter le MVP à d'autres produits facilement
+const APIURL = "http://localhost:3000/api/" + productSold + "/";
+
 /*L'utilisateur à besoin d'un panier dans le localStorage de son navigateur
 Vérifier si le panier existe dans le localStorage, sinon le créer et l'envoyer dans le localStorage au premier chargement du site quelque soit la page*/
 
@@ -18,60 +21,63 @@ let userCart = JSON.parse(localStorage.getItem("userCart"));
 // ---------------------------------
 
 function getProducts() {
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function() {
-		if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-			var response = JSON.parse(this.responseText);
+	fetch(APIURL)
+		.then(
+			function (response) {
+				if (response.status !== 200) {
+					console.log('Erreur, code : ' + response.status);
+					return;
+				}
 
-			console.log(response);
+				// Analyse de la réponse
+				response.json().then(function(data) {
+					console.log(data);
 
-			var productsContainer = document.getElementById("products-container");
-			response.forEach((item) => {
-				// Product container
-				var prodContainer = document.createElement("div");
-				prodContainer.setAttribute("id",item._id);
-				prodContainer.setAttribute("class","product-container p-2");
+					var productsContainer = document.getElementById("products-container");
+					data.forEach((item) => {
+						// Product container
+						var prodContainer = document.createElement("div");
+						prodContainer.setAttribute("id",item._id);
+						prodContainer.setAttribute("class","product-container p-2");
 
-				// Product image
-				var prodImgContainer = document.createElement("div");
-				prodImgContainer.setAttribute("id",item._id+"-img");
-				prodImgContainer.setAttribute("class","img-wrapper");
+						// Product image
+						var prodImgContainer = document.createElement("div");
+						prodImgContainer.setAttribute("id",item._id+"-img");
+						prodImgContainer.setAttribute("class","img-wrapper");
 
-				var prodImg = document.createElement("img");
-				prodImg.setAttribute("src",item.imageUrl);
+						var prodImg = document.createElement("img");
+						prodImg.setAttribute("src",item.imageUrl);
 
-				// Product name
-				var prodName = document.createElement("a");
-				prodName.setAttribute("class","product-link");
-				prodName.setAttribute("href","product.html?id="+item._id);
-				prodName.innerHTML = item.name;
+						// Product name
+						var prodName = document.createElement("a");
+						prodName.setAttribute("class","product-link");
+						prodName.setAttribute("href","product.html?id="+item._id);
+						prodName.innerHTML = item.name;
 
-				// Product price
-				var prodPrice = document.createElement("h3");
-				var price = (item.price/100).toFixed(2);
-				prodPrice.innerHTML = price+"€";
+						// Product price
+						var prodPrice = document.createElement("h3");
+						var price = (item.price/100).toFixed(2);
+						prodPrice.innerHTML = price+"€";
 
-				// Append every element to the div
-				productsContainer.append(prodContainer);
-				var prodContainerId = document.getElementById(item._id);
-				prodContainerId.append(prodImgContainer);
+						// Append every element to the div
+						productsContainer.append(prodContainer);
+						var prodContainerId = document.getElementById(item._id);
+						prodContainerId.append(prodImgContainer);
 
-				var prodImgContainerId = document.getElementById(item._id+"-img");
-				prodImgContainerId.append(prodImg);
+						var prodImgContainerId = document.getElementById(item._id+"-img");
+						prodImgContainerId.append(prodImg);
 
-				prodContainerId.append(prodName);
-				prodContainerId.append(prodPrice);
-			});
-			// console.error(response);
-			// console.log("Connection à l'API réussie");
-			apiErrors.innerHTML = "";
-		} else {
-			// console.log("Erreur de connexion à l'API");
-			apiErrors.innerHTML = '<div class="error">Nous sommes désolé, il y a une erreur lors de la connexion à l\'API.<br>Veuillez vérifier son état.</div>';
-		}
-	};
-	request.open("GET", "http://localhost:3000/api/cameras");
-	request.send();
+						prodContainerId.append(prodName);
+						prodContainerId.append(prodPrice);
+					});
+					// console.log("Connection à l'API réussie");
+					apiErrors.innerHTML = "";
+				});
+			}
+		)
+		.catch(function(err) {
+			console.log('Erreur fetch :-S', err);
+		});
 }
 
 function getProductInfos() {
@@ -121,7 +127,7 @@ function getProductInfos() {
 			productInformations.style.display = "none";
 		}
 	};
-	request.open("GET", "http://localhost:3000/api/cameras/"+ idProduit);
+	request.open("GET", APIURL + idProduit);
 	request.send();
 }
 
@@ -164,7 +170,7 @@ function getProductById(idProduit) {
 			apiErrors.innerHTML = '<div class="error">Nous sommes désolé, il y a une erreur lors de la connexion à l\'API.<br>Veuillez vérifier son état.</div>';
 		}
 	};
-	request.open("GET", "http://localhost:3000/api/cameras/"+ idProduit);
+	request.open("GET", APIURL + idProduit);
 	request.send();
 }
 
@@ -213,7 +219,7 @@ function checkCart() {
 					cartContainer.innerHTML += "<div class=\"row\">"+chain+"</div>";
 				}
 			};
-			request.open("GET", "http://localhost:3000/api/cameras/"+ item);
+			request.open("GET", APIURL + item);
 			request.send();
 		});
 	}
@@ -272,7 +278,7 @@ function checkout() {
 						document.forms["order-form"].submit();
 					}
 				};
-			request.open("POST", "http://localhost:3000/api/cameras/order");
+			request.open("POST", APIURL + "order");
 			request.setRequestHeader("Content-Type", "application/json");
 			console.log(data);
 			request.send(object);
